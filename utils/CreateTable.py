a2035b4b02f7c3bbfac4ed26c57c036bda6bd4f8
StevenNDev <steve.dev.business@proton.me>
2024-03-04 17:37:39 +0700

    CreateTable added!

---
+++ utils/CreateTable.py
@@ -0,0 +1,22 @@
+from Connector import Connect
+from progressbar import progressbar
+
+# Creating Table:
+class CreateTable(Connect):
+    def __init__(self):
+        DB_name = input("Enter Database Name: ")
+        super().__init__(DB_name)
+
+    # Using transaction
+    def exec(self, statement, params=None):
+        cursor = super().Csr()
+        try:
+            cursor.execute("BEGIN TRANSACTION")
+            if params:
+                cursor.execute(statement, params)
+            else:
+                cursor.execute(statement)
+            cursor.execute("COMMIT")
+        except Exception as e:
+            cursor.execute("ROLLBACK")
+            print("An Error Occurred: ", e)
