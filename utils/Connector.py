53b549d75a836b1e8a633d0b40ec82edfa56a850
StevenNDev <steve.dev.business@proton.me>
2024-03-09 16:06:49 +0700

    Connector.py now defined as main of sub function sql statememt!

--- utils/Connector.py
+++ utils/Connector.py
@@ -1,8 +1,8 @@
+from .Design import Art
+from .Engine import Engine
 import sqlite3 as sql
 import os
-from time import sleep
-from .Design import Art
-
+import time
 
 class Connect:
     """
@@ -12,36 +12,38 @@ class Connect:
         __init__(db: str)
             Initializes the Connect object and establishes a connection to the database.
 
-        Csr()
+        cursor()
             Returns a cursor object for executing SQL commands on the connected database.
     """
 
-    def __init__(self, db: str):
-        """
-        Initializes the Connect object and establishes a connection to the database.
-
-        Args:
-            db (str): Path to the database file.
-        """
-        self.db = db
+    def __init__(self, db=None):
+        self.engine = Engine()
         self.art = Art()
-        sleep(1)
+        self.conn = None  # Initialize self.conn
+        self.db = db if db else self._prompt_database_path()
+        
+    def cursor(self):  # Correct method name to lowercase
+        time.sleep(1)
         if os.path.exists(self.db):
             self.art.Header("AccountingApp V2")
-            self.art.Loading("Connecting Into Database...", 4)
+            self.art.Loading("Connecting Into Database...", 2)
             self.conn = sql.connect(self.db)
             print()
             self.art.ColorPrint("Successfully Connected Into Database!", "green")
             print()
-            sleep(1)
+            time.sleep(1)
+            self.art.Loading("Cursoring data....", 3)
+            self.art.ColorPrint("Done!\n", 'green')
+            time.sleep(1)
+            return self.conn.cursor()  # Return the cursor object
         else:
             raise FileNotFoundError(self.art.ColorPrint("File Not Found!", "red"))
 
-    def Csr(self):
+    def _prompt_database_path(self):
+         return self.engine.fprompt("Enter Database Name: ", "db")
+ 
+    def close_connection(self):
         """
-        Returns a cursor object for executing SQL commands on the connected database.
-
-        Returns:
-            Cursor: Cursor object for executing SQL commands.
+        Closes the connection to the database.
         """
-        return self.conn.cursor()
+        self.conn.close()
