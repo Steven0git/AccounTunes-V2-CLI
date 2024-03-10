c1435cdf0982187c0b9f635a4a408a248f5ef3d3
StevenNDev <steve.dev.business@proton.me>
2024-03-10 13:22:22 +0700

    select_menu() added in Engine.py

--- utils/artisan/Engine.py
+++ utils/artisan/Engine.py
@@ -1,12 +1,13 @@
-from .Design import Art
+from Design import Art
 from time import sleep
 import re
 import os
+import sys
 
 
 class Engine:
     """
-    Engine Class with file-related methods.
+    Engine class containing file-related methods.
     """
 
     def __init__(self):
@@ -15,11 +16,52 @@ class Engine:
         """
         self.art = Art()
 
-    def prompt(self, args) -> str:
+    def prompt(self, args: str) -> str:
         """
         Prompts the user.
+
+        Args:
+            args (str): Arguments for the prompt.
+
+        Returns:
+            str: User input.
         """
         pass
+
+    def select_menu(self, menu_available: list) -> int:
+        """
+        Displays the menu and prompts the user to select an option.
+
+        Args:
+            menu_available (list): List of menu options.
+
+        Returns:
+            int: Selected menu option.
+        """
+        while True:
+            self.art.print_color("Select Menu:", "yellow", " ")
+            try:
+                menu_selected = int(input())
+                if 1 <= menu_selected <= len(menu_available):
+                    self.art.print_color(
+                        f"\tSelected {menu_available[menu_selected-1]}", "green"
+                    )
+                    self.art.print_color("\nAre you sure (yes/no/quit):", "yellow", " ")
+                    confirmation = input().lower()
+                    if confirmation in ["yes", "y"]:
+                        self.art.print_color("\tConfirmed!", "green")
+                        return menu_selected
+                    elif confirmation in ["q", "quit"]:
+                        self.art.print_color("\tTransaction cancelled.", "red")
+                        sys.exit(1)
+                    else:
+                        self.error_message("Selection canceled.", False)
+                else:
+                    self.error_message("Wrong Selection!", False)
+            except ValueError:
+                self.error_message(
+                    "Invalid input! Please enter a valid integer.", False
+                )
 
     def fprompt(self, prompt_msg: str, filetype: str) -> str:
         """
@@ -57,27 +99,26 @@ class Engine:
         pattern = re.compile(rf"^[a-zA-Z0-9_]+\.{filetype}$")
         return bool(pattern.match(filename))
 
-    def error_message(self, msg: str):
+    def error_message(self, msg: str, clean_screen: bool = True):
         """
         Displays an error message.
 
         Args:
             msg (str): The error message to display.
+            clean_screen (bool): Whether to clear the screen after displaying the message.
         """
         if len(msg.strip()):
             self.art.print_color(f"{msg}\n", "red")
         else:
-            self.art.print_color("You're an asshole! add some words!\n", "red")
-        sleep(0.8)
-        os.system("cls" if os.name == "nt" else "clear")
+            self.art.print_color("Error: Message not provided.\n", "red")
+        if clean_screen:
+            sleep(0.8)
+            os.system("cls" if os.name == "nt" else "clear")
 
     @property
     def SuppressError(self):
-        import os
-        import sys
-
         """
-        Initializes the SuppressError class.
+        Suppresses error messages.
         """
         try:
             null_device = "NUL" if os.name == "nt" else os.devnull
