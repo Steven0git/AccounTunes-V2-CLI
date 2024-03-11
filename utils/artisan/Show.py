e0e993bb8464a1540bf704ede4406f6570da2756
StevenNDev <steve.dev.business@proton.me>
2024-03-11 16:23:35 +0700

    Show.py used for database handling

---
+++ utils/artisan/Show.py
@@ -0,0 +1,17 @@
+from Design import Art
+from time import sleep
+
+class Show:
+  def __init__(self):
+    self.art = Art()
+    self.print = self.art.print_color
+    
+  def readable(self, data: dict):
+    self.print("_"*40, "MAGENTA")
+    self.print("Your saved data: \n", "GREEN")
+    for index,(key, value) in enumerate(data.items()):
+      self.print(f"\t{index+1}: {key}", "yellow", "")
+      self.print(" --> ", "RED", "")
+      self.print(value, "green")
+      sleep(0.1)
+    self.print("_"*40, "MAGENTA")
